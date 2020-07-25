34a6a6eb52a3ced261e8d36ce1987b893648d6e1
System-in-a-System <textconstruction@protonmail.com>
2020-07-25 12:39:01 +0300

    Adding class Player

---
+++ Hangman-Game/Hangman-Game/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_Game
+{
+    class Player
+    {
+        /** Player's name. */
+        private string name;
+
+        /** Player's score. */
+        private int score = 0;
+
+
+
+        /**
+         * Instantiates a new player.
+         *
+         * @param nameInput - the name of the player 
+         */
+        public Player(String nameInput)
+        {
+            setName(nameInput);
+        }
+
+
+
+        /** Getters and Setters for private data fields: */
+
+        /**
+         * Gets the name.
+         *
+         * @return the name
+         */
+        public string Name()
+        {
+            return name;
+        }
+
+
+        /**
+         * Sets the name.
+         *
+         * @param nameInput the new name
+         */
+        public void setName(string nameInput)
+        {
+            if (nameInput != "")
+                name = nameInput;
+            else
+                name = "Unknown hero";
+        }
+
+
+
+        /**
+         * Records player's score (establishes player-score pair)
+         *
+         * @param theScore = the current score of the current player within the current round
+         */
+        public void recordScore(int theScore) 
+        {
+			score = theScore;
+        }
+
+
+
+        /**
+	    * Retrieves player's score (established in player-score pair)
+	    *
+	    * @return the int value of the player's score
+	    */
+        public int retrieveScore()
+        {
+            return score;
+        }
+    }
+}
